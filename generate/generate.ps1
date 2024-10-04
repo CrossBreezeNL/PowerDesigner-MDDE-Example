e0e121599fb3a001cef6c05289e246f01c9786c9
Richane <richane@crossbreeze.nl>
2024-10-04 17:06:30 +0200

    Created MDDE_Example PDM Model file + setup generation for ADF

--- generate/generate.ps1
+++ generate/generate.ps1
@@ -3,4 +3,6 @@ $RepoRoot = Split-Path -parent $PSScriptRoot
 $XGenJar = "$RepoRoot\lib\CrossBreeze.CrossGenerate\XGenerate.jar"
 $XgGenCmd = "java -jar $XGenJar"
 
-cmd /c $XgGenCmd -cll INFO -d true -c .\XGenAppConfig.xml -mtc "../models/decomposed/MDDE_Example/MDDE_Example.ldm::Model-Code/Model-Code.database.xml::ModelXmlConfig.xml"
+cmd /c $XgGenCmd -cll INFO -c .\XGenAppConfig.xml `
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.ldm::Model-Code/Model-Code.database.xml::ModelXmlConfig.xml" `
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Pipeline/PipelineCopyData_Table_Code.json::PipelineADFXmlConfig.xml"
