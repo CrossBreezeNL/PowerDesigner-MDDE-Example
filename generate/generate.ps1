2a1f3ca260226f9a9fd35d071a410627cb0ab6f9
Richane <richane@crossbreeze.nl>
2024-10-11 16:16:25 +0200

    Added TableTextConfig to generate Table_Code.SQL. Also modified the Table_Code.sql to make the generation work.

--- generate/generate.ps1
+++ generate/generate.ps1
@@ -6,4 +6,5 @@ $XgGenCmd = "java -jar $XGenJar"
 cmd /c $XgGenCmd -cll INFO -c .\XGenAppConfig.xml `
     -mtc "../models/decomposed/MDDE_Example/MDDE_Example.ldm::Model-Code/Model-Code.database.xml::ModelXmlConfig.xml" `
     -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Pipeline/PipelineCopyData_Table_Code.json::PipelineADFXmlConfig.xml" `
-    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Database/Schema_Code/Schema_Code.sql::SchemaTextConfig.xml"
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Database/Schema_Code/Schema_Code.sql::SchemaTextConfig.xml" `
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Database/Schema_Code/Tables/Table_Code.sql::TableTextConfig.xml"
--- generate/template/Database_Code/Database/Schema_Code/Tables/Table_Code.sql
+++ generate/template/Database_Code/Database/Schema_Code/Tables/Table_Code.sql
@@ -1,5 +1,7 @@
+-- @XGenTextSection(name="Table" literalOnLastLine = "--end of table")
 CREATE TABLE [Schema_Code].[Table_Code]
 (
-  -- @XGenTextSection(name='Column' suffix=',')
+  -- @XGenTextSection(name="Column" suffix=",")
   [Column_Code] Column_SqlDataType
 );
+--end of table
