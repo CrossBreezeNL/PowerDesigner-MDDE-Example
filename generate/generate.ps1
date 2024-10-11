b4273e894d89bf74a7cff5a457668c67597528dc
Richane <richane@crossbreeze.nl>
2024-10-11 11:09:56 +0200

    Adjusted PipelineADF name to Table and added the SchemaTextConfig file to generate a Create Schema file. Also added a new mtc command to generate the Schema SQL

--- generate/generate.ps1
+++ generate/generate.ps1
@@ -5,4 +5,5 @@ $XgGenCmd = "java -jar $XGenJar"
 
 cmd /c $XgGenCmd -cll INFO -c .\XGenAppConfig.xml `
     -mtc "../models/decomposed/MDDE_Example/MDDE_Example.ldm::Model-Code/Model-Code.database.xml::ModelXmlConfig.xml" `
-    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Pipeline/PipelineCopyData_Table_Code.json::PipelineADFXmlConfig.xml"
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Pipeline/PipelineCopyData_Table_Code.json::PipelineADFXmlConfig.xml" `
+    -mtc "../models/decomposed/MDDE_Example/MDDE_Example.xml::Database_Code/Database/Schema_Code/Schema_Code.sql::SchemaTextConfig.xml"
