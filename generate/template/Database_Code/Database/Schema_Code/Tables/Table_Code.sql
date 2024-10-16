e3fec2d464f6869b5529424336cc12275c0b3761
Richane <richane@crossbreeze.nl>
2024-10-16 15:36:25 +0200

    Table Code gets generated per table and not in one file anymore

--- generate/template/Database_Code/Database/Schema_Code/Tables/Table_Code.sql
+++ generate/template/Database_Code/Database/Schema_Code/Tables/Table_Code.sql
@@ -1,7 +1,6 @@
--- @XGenTextSection(name="Table" literalOnLastLine = "--end of table")
 CREATE TABLE [Schema_Code].[Table_Code]
 (
   -- @XGenTextSection(name="Column" suffix=",")
   [Column_Code] Column_SqlDataType
 );
---end of table
+
