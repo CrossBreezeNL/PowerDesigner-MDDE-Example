49e341ab93c17843ab7f5421b036924150d3482b
Harmen Wessels <harmen@x-breeze.com>
2024-11-28 11:48:14 +0100

    Added template for pdm based on model XML export. Updated generate script to first generate pdm file, and then generate using pdm file as model.

--- generate/generate.ps1
+++ generate/generate.ps1
@@ -17,29 +17,35 @@ if (Test-Path $TargetFolder) {
 $TargetDirectoryObj = New-Item -Path $TargetFolder -ItemType "directory"
 Write-Host "[$(Get-Date -format 'yyyy-MM-dd HH:mm:ss')] [INFO] Created target directory"
 
+# Run the generation process for the Database metadata file.
+Write-Host "[$(Get-Date -format 'yyyy-MM-dd HH:mm:ss')] [INFO] Generating database metadata file"
+cmd /c $XgGenCmd `
+    -cll WARNING `
+    -fll INFO `
+    -fld $TargetLogFile `
+    -c .\XGenAppConfig.xml `
+    -mtc "MDDE_Example/MDDE_EXAMPLE.model.xml::PhysicalModel/Model_Code.pdm.xml::ModelXmlConfig.xml"
+
 # Run the generation process.
-Write-Host "[$(Get-Date -format 'yyyy-MM-dd HH:mm:ss')] [INFO] Starting CrossGenerate"
+Write-Host "[$(Get-Date -format 'yyyy-MM-dd HH:mm:ss')] [INFO] Generating target artifacts"
 # -cll: Console log level
 # -fll: File log level
-# -fld: File log destionation
+# -fld: File log destination
 # -c: CrossGenerate Application config location.
 # -mtc: The Model::Template::Config combinations to generate for.
 cmd /c $XgGenCmd `
     -cll WARNING `
     -fll INFO `
     -fld $TargetLogFile `
-    -c .\XGenAppConfig.xml `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Pipeline/PipelineCopyData_Table_Code.json::PipelineADFXmlConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Database_Code/Database_Code.sqlproj::DatabaseXmlConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Database_Code/Schema_Code/Schema_Code.sql::SchemaTextConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Database_Code/Schema_Code/Tables/Table_Code.sql::TableTextConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Staging_Database_Code/Staging_Database_Code.sqlproj::DatabaseXmlConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Staging_Database_Code/Schema_Code/Staging_Schema_Code.sql::SchemaTextConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Staging_Database_Code/Schema_Code/Tables/Table_Code.sql::TableTextConfig.xml" `
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Database_Code.sln::SolutionTextConfig.xml"`
-    -mtc "MDDE_Example/MDDE_Example.xml::Database/Pipeline/Airflow.py::AirflowTextConfig.xml"
-    # TODO: The generation of the Database XML file is disabled for now, since there is an issue with namespaces.
-    #-mtc "MDDE_Example/MDDE_Example.ldm::Model-Code/Model-Code.database.xml::ModelXmlConfig.xml" 
+    -c .\XGenTargetAppConfig.xml `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Database_Code/Database_Code.sqlproj::DatabaseXmlConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Database_Code/Schema_Code/Schema_Code.sql::SchemaTextConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Database_Code/Schema_Code/Tables/Table_Code.sql::TableTextConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Staging_Database_Code/Staging_Database_Code.sqlproj::DatabaseXmlConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Staging_Database_Code/Schema_Code/Staging_Schema_Code.sql::SchemaTextConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Staging_Database_Code/Schema_Code/Tables/Table_Code.sql::TableTextConfig.xml" `
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Database_Code.sln::SolutionTextConfig.xml"`
+    -mtc "MDDE_EXAMPLE.pdm.xml::Database/Pipeline/Airflow.py::AirflowTextConfig.xml"
 
 # Check whether the generation ended with an error.
 if ($LASTEXITCODE -eq 1) {
